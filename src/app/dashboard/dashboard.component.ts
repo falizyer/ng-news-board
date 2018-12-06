f81aba6f93e8105fe7d6d935f284af089180775b
falizyer <falizyer@outlook.com>
2018-12-06 18:41:30 +0300

    pagination ph1

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { max } from 'rxjs/operators';
 
 @Component({
   selector: 'nb-dashboard',
@@ -9,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class DashboardComponent implements OnInit {
 
   private sources = [];
+  private pageArray: Array<number>;
 
   constructor(private route: ActivatedRoute) {
   }
@@ -18,7 +20,13 @@ export class DashboardComponent implements OnInit {
     this.route.params.subscribe(params => {
       const index: number = +params['index'];
       const start: number = (index - 1) * 10;
+      const numberOfPages: number = Math.floor(sources.sources.length / 10);
+      const minPage: number = Math.max(1, start - 3);
+      const maxPage: number = Math.min(numberOfPages, start + 3);
+      this.pageArray = new Array(maxPage - minPage);
+      this.pageArray.fill(0);
       this.sources = sources.sources.slice(start, index * 10);
+      this.pageArray = this.pageArray.map((d, i) => minPage + i);
     });
   }
 }
