1309496bda6689b601d1d392770390fc7d67c429
Dzmitry Hrachou <falizyer@outlook.com>
2018-12-12 15:52:50 +0300

    + url language switcher added

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -15,7 +14,6 @@ import { FilterPipe } from './filter.pipe';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule,
     HttpClientModule,
     SharedModule
   ],
